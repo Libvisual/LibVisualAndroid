8827cc4c28ad5cf270a37569d760d60d34881a97
daniel <info-lvand@boogiepalace.hopto.org>
2012-04-08 06:09:26 +0200

    added input_debug, all morph plugins and some actor plugins

--- jni/libvisual/libvisual-plugins/plugins/actor/lv_analyzer/Android.mk
+++ jni/libvisual/libvisual-plugins/plugins/actor/lv_analyzer/Android.mk
@@ -1,4 +1,5 @@
 LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
 
 LOCAL_C_INCLUDES += $(LV_C_INCLUDES)
@@ -6,10 +7,10 @@ LOCAL_CFLAGS     += $(LV_CFLAGS)
 LOCAL_CXXFLAGS   += $(LV_CXXFLAGS)
 LOCAL_CPPFLAGS   += $(LV_CPPFLAGS)
 
-
-LOCAL_MODULE           := lvclient
-LOCAL_SRC_FILES        := lvclient.c stats.c
+LOCAL_MODULE           := actor_lv_analyzer
+LOCAL_SRC_FILES        := \
+	$(addprefix /, $(notdir $(wildcard $(LOCAL_PATH)/*.c) $(wildcard $(LOCAL_PATH)/*.cpp)))
+LOCAL_LDLIBS           +=
 LOCAL_SHARED_LIBRARIES += visual
-LOCAL_LDLIBS           += -lm -llog -ljnigraphics
 
 include $(BUILD_SHARED_LIBRARY)
--- jni/libvisual/libvisual/libvisual/Android.mk
+++ jni/libvisual/libvisual/libvisual/Android.mk
@@ -11,8 +11,8 @@ LOCAL_MODULE           := visual
 LOCAL_SRC_FILES        := \
 	$(addprefix /, $(notdir $(wildcard $(LOCAL_PATH)/*.c) $(wildcard $(LOCAL_PATH)/*.cpp))) \
 	$(addprefix /private/, $(notdir $(wildcard $(LOCAL_PATH)/private/*.c) $(wildcard $(LOCAL_PATH)/private/*.cpp)))
-LOCAL_LDLIBS           := -ldl -lm -llog -ljnigraphics
-LOCAL_SHARED_LIBRARIES := cpufeatures
+LOCAL_LDLIBS           += -ldl -lm -llog -ljnigraphics
+LOCAL_SHARED_LIBRARIES += cpufeatures
 
 include $(BUILD_SHARED_LIBRARY)
 $(call import-module,cpufeatures)
