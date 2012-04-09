102455bf2125cd03a04616e8ae6b5e5219fec374
daniel <info-lvand@boogiepalace.hopto.org>
2012-04-09 09:31:37 +0200

    added some more actors (not all of them work yet)

---
+++ jni/libvisual/libvisual-plugins/plugins/actor/jakdaw/Android.mk
@@ -0,0 +1,16 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_C_INCLUDES += $(LV_C_INCLUDES)
+LOCAL_CFLAGS     += $(LV_CFLAGS)
+LOCAL_CXXFLAGS   += $(LV_CXXFLAGS)
+LOCAL_CPPFLAGS   += $(LV_CPPFLAGS)
+
+LOCAL_MODULE           := actor_jakdaw
+LOCAL_SRC_FILES        := \
+	$(addprefix /, $(notdir $(wildcard $(LOCAL_PATH)/*.c) $(wildcard $(LOCAL_PATH)/*.cpp)))
+LOCAL_LDLIBS           +=
+LOCAL_SHARED_LIBRARIES += visual
+
+include $(BUILD_SHARED_LIBRARY)
