a7a9d8c3bbacd3954257d15a8b1b5281cd1850ee
daniel <info-lvand@boogiepalace.hopto.org>
2012-04-10 12:29:36 +0200

    added some more variables to build

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,15 +1,18 @@
 
-WARNING_CFLAGS := -Wall -Wstrict-aliasing -Wcast-align -Waddress -Wmissing-braces -Wimplicit -Wunused -Wno-unused-variable
+WARNING_CFLAGS 		:= -Wall -Wstrict-aliasing -Wcast-align -Waddress -Wmissing-braces -Wimplicit -Wunused -Wno-unused-variable
 
-LV_C_INCLUDES  := jni/libvisual/libvisual
-LV_CFLAGS      := $(WARNING_CFLAGS)
-LV_CXXFLAGS    :=
-LV_CPPFLAGS    := -std=gnu++0x
-
+LV_C_INCLUDES  		:= jni/libvisual/libvisual
+LV_CFLAGS      		:= $(WARNING_CFLAGS)
+LV_CXXFLAGS    		:=
+LV_CPPFLAGS    		:= -std=gnu++0x
+LV_SHARED_LIBRARIES	:= 
+LV_STATIC_LIBRARIES	:=
+LV_LDFLAGS              :=
+LV_LDLIBS               :=
 
 
 ifeq ($(TARGET_ARCH_ABI),armeabi)
-#    LV_CFLAGS += -march=armv6 -mfp=vfp -D__ARM_ARCH_6__
+#    LV_CFLAGS += -D__ARM_ARCH_6__ -march=armv6-m 
 endif
         
 ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
