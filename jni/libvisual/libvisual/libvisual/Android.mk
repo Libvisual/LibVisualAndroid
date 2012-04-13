0a5d9a1bdb0adb398472e7f60fea5277a4af0d77
daniel <info-lvand@boogiepalace.hopto.org>
2012-04-13 21:53:59 +0200

    added files in /libvisual/private/posix to build

--- jni/libvisual/libvisual/libvisual/Android.mk
+++ jni/libvisual/libvisual/libvisual/Android.mk
@@ -15,7 +15,8 @@ LOCAL_STATIC_LIBRARIES  += $(LV_STATIC_LIBRARIES)
 LOCAL_MODULE            := visual
 LOCAL_SRC_FILES         := \
 	$(addprefix /, $(notdir $(wildcard $(LOCAL_PATH)/*.c) $(wildcard $(LOCAL_PATH)/*.cpp))) \
-	$(addprefix /private/, $(notdir $(wildcard $(LOCAL_PATH)/private/*.c) $(wildcard $(LOCAL_PATH)/private/*.cpp)))
+	$(addprefix /private/, $(notdir $(wildcard $(LOCAL_PATH)/private/*.c) $(wildcard $(LOCAL_PATH)/private/*.cpp))) \
+    $(addprefix /private/posix/, $(notdir $(wildcard $(LOCAL_PATH)/private/posix/*.c) $(wildcard $(LOCAL_PATH)/private/posix/*.cpp)))
 LOCAL_LDLIBS            += -ldl -lm -llog
 LOCAL_SHARED_LIBRARIES  += cpufeatures
 
